4b9b70a7c1b6f2d577a17d47195d0a00118c7a7e
Raphael Americano <raphaelamericano@Raphaels-MacBook-Pro.local>
2022-10-05 15:03:37 -0300

    fix header layout

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -13,17 +13,19 @@ export default function Header(props: IProps) {
   const { title, menu } = props;
   return (
     <header className={scss.header}>
-      <div>
-        <h1 className={scss.heading}>{title}</h1>
+      <div className={scss.container}>
+        <div>
+          <h1 className={scss.heading}>{title}</h1>
+        </div>
+        {menu && <nav className={scss.nav}>
+          <ul>
+              {menu.map((item) => <li key={item.text}>
+                  { item.to ? <Link to={item.to} className={scss.navItem}>{item.text}</Link> : <button onClick={item.clickFn} className={scss.navItem}>{item.text}</button> }
+                  
+                  </li> )}
+          </ul>
+          </nav>}
       </div>
-      {menu && <nav className={scss.nav}>
-        <ul>
-            {menu.map((item) => <li key={item.text}>
-                { item.to ? <Link to={item.to} className={scss.navItem}>{item.text}</Link> : <button onClick={item.clickFn} className={scss.navItem}>{item.text}</button> }
-                
-                </li> )}
-        </ul>
-        </nav>}
 
     </header>
   );
