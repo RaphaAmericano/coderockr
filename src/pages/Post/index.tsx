b478feaf285a757732e21d68a3ae1caa18254cbc
Raphael Americano <raphaelamericano@Raphaels-MacBook-Pro.local>
2022-10-05 16:01:58 -0300

    post page desktop display

--- src/pages/Post/index.tsx
+++ src/pages/Post/index.tsx
@@ -1,5 +1,36 @@
+import { useParams } from "react-router-dom"
+import { postQueries } from "../../hooks/queries";
+import { dateHelpers } from "../../helpers";
 
-export default function Posts(){
+import scss from "./style.module.scss";
+
+export default function Post(){
+    const { id } = useParams<{id: string }>();
+
+    const { data } = postQueries.useGetPosts({});
+
+    if(!data) return null;
     
-    return <div>Posts</div>
+    const postData = data.find((post) => post.id === id);
+    if(!postData) return null;
+
+    const { title, article, imageUrl, date, author, authorEmail } = postData;
+    
+    return <main className={scss.main}>
+            <section className={scss.container}>
+                <header className={scss.header}>
+                    <div className={scss.imageBlock}>
+                        <img src={imageUrl}/>
+                    </div>
+                    <div className={scss.textBlock}>
+                        <span className={scss.date}>{dateHelpers.postDateString(date)}</span>
+                        <span className={scss.author}>
+                            <a href={`mailto:${authorEmail}`}>{author}</a>
+                        </span>
+                        <h1>{title}</h1>
+                    </div>
+                </header>
+                <article className={scss.article} dangerouslySetInnerHTML={ {__html: article }}/>
+            </section>
+        </main>
 }
