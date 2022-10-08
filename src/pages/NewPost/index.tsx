a5bc70114cd68b310b2cb29482e7b9c14c7cf595
Raphael Americano <raphaelamericano@Raphaels-MacBook-Pro.local>
2022-10-08 20:33:15 -0300

    post page boiler and header fix

---
+++ src/pages/NewPost/index.tsx
@@ -0,0 +1,10 @@
+import { postQueries } from "../../hooks/queries";
+import scss from "./style.module.scss";
+
+export default function NewPost(){
+    return <main className={scss.main}>
+        <section>
+            
+        </section>
+    </main>   
+}
