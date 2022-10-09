3160ee354a46932049087b97342c7f8c10990726
RaphaAmericano <RaphaAmericano@users.noreply.github.com>
2022-10-09 17:36:18 +0000

    deploy: 1cc45c531cd8b4a11bd91a9b1ec830860c16741e

--- assets/index.d3e7c362.js
+++
@@ -1,1 +0,0 @@
-import{a as n}from"./index.de204c33.js";const s="_main_1sdgf_1",a="_container_1sdgf_13",t={main:s,container:a};function o(){return n("main",{className:t.main,children:n("section",{})})}export{o as default};
